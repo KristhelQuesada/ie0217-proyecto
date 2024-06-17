6bc06c53b2d27df23107f621fd94106568db0c78
anthonybr14 <161529475+anthonybr14@users.noreply.github.com>
2024-06-16 20:13:20 -0600

    Actualizando clases Cliente, Cuenta, Transferencia.
    Signed-off-by: anthonybr14 <161529475+anthonybr14@users.noreply.github.com>

--- src/Cliente.hpp
+++ src/Cliente.hpp
@@ -6,15 +6,52 @@
 #include <string>
 #include <iostream>
 
+#include "DBManager.hpp"
+
 template<typename T>
 class Cliente {
 private:
-    std::unordered_map<std::string, T> informacion;
-    
+  std::map<std::string, T> informacion; // Mapa para almacenar la información del cliente
+
 public:
-    Cliente(int idCliente, const std::string& nombre, const std::string& apellido);
-    void obtenerInformacion();
-    void actualizarDatos();
+  // Constructor para inicializar un cliente
+  Cliente(int idCliente, const std::string& nombre, const std::string& apellido, int idCuentaColones, int idCuentaDolares);
+
+  // Métodos para obtener y establecer el ID del cliente
+  T getIdCliente() const;
+  void setIdCliente(T idCliente);
+
+  // Métodos para obtener y establecer el nombre del cliente
+  const std::string& getNombreCliente() const;
+  void setNombreCliente(const std::string& nombre);
+
+  // Métodos para obtener y establecer el apellido del cliente
+  const std::string& getApellidoCliente() const;
+  void setApellidoCliente(const std::string& apellido);
+
+  // Métodos para obtener y establecer el ID de la cuenta en colones
+  T getIdCuentaColones() const;
+  void setIdCuentaColones(T idCuentaColones);
+
+  // Métodos para obtener y establecer el ID de la cuenta en dólares
+  T getIdCuentaDolares() const;
+  void setIdCuentaDolares(T idCuentaColones);
+
+  // Método para obtener toda la información del cliente
+  std::map<std::string, T> getInformacion() const;
+
+  // Método para solicitar datos al usuario e inicializar el objeto
+  void obtenerInformacion();
+
+  // Función para insertar datos en la base de datos
+  bool insertarInformacionBaseDeDatos(DBManager& dbManager);
+  
+  // Método para actualizar nombre y apellido del cliente
+  void actualizarDatos(DBManager& dbManager);
+
+  // Método para eliminar datos del cliente
+  bool elminarInformacionBaseDeDatos(DBManager& dbManager);
+  
 };
 
 #endif // CLIENTE_HPP
--- src/Transferencia.hpp
+++ src/Transferencia.hpp
@@ -1,6 +1,7 @@
 #ifndef TRANSFERENCIA_HPP
 #define TRANSFERENCIA_HPP
 
+#include <cmath>
 #include "Transaccion.hpp"
 
 class Transferencia : public Transaccion {
@@ -11,10 +12,34 @@ private:
     double balancePosterior;
 
 public:
+    // Constructor
     Transferencia(int id, double monto, const std::string& moneda, const std::string& detalle,
            int cuentaOrigen, int cuentaDestino, double balanceAnterior, double balancePosterior);
 
-    void ejecutar() override; // Implementa el método de transferencia
+    // Implementa el método de transferencia
+    void ejecutar() override;
+
+    // Método que solicita información
+    void solicitudInformacion ();
+
+    // Método para convertir moneda
+    double convertirMoneda(double monto, const std::string& monedaOrigen, 
+                        const std::string& monedaDestino);
+
+    // Métodos para transferir entre cuentas de la misma moneda y diferente moneda
+    void transferirMismaMoneda(double monto, const std::string& moneda, 
+                             int numeroCuentaOrigen, int numeroCuentaDestino);
+    void transferirDiferenteMoneda(double monto, const std::string& monedaDestino, 
+                                 int numeroCuentaOrigen, int numeroCuentaDestino);
+
+    // Métodos para consultar y actualizar saldos
+    double consultarSaldo(int numeroCuenta);
+    void actualizarSaldos(int numeroCuenta, double nuevoSaldo);
+
+    // Método para registrar la transacción
+    void registrarTransaccion(int idCliente, const std::string& nombreCliente, int numeroCuentaOrigen, 
+                            int numeroCuentaDestino, double monto, const std::string& moneda, 
+                            const std::string& detalle);
 };
 
-#endif // TRANSFERENCIA_HPP
+#endif // TRANSFERENCIA_HPP 
